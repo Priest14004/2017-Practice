dc5629ac7bea2c156fb473d80d08e35c91e16fdc
Sam Fuchs <samcfuchs@gmail.com>
2017-01-11 20:42:53 -0500

    Use registrar for turret

--- src/main/java/frc/team236/testbed/subsystems/Turret.java
+++ src/main/java/frc/team236/testbed/subsystems/Turret.java
@@ -2,10 +2,10 @@ package frc.team236.testbed.subsystems;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.team236.testbed.RobotMap;
 import frc.team236.testbed.commands.ManualTurret;
+import jaci.openrio.toast.lib.registry.Registrar;
 //
 //
 //
@@ -22,8 +22,8 @@ public class Turret extends Subsystem {
 	 */
 	public Turret() {
 		// Instantiate motors
-		yawMotor = new Victor(RobotMap.Turret.PWM_YAW);
-		pitchMotor = new Victor(RobotMap.Turret.PWM_PITCH);
+		yawMotor = Registrar.victor(RobotMap.Turret.PWM_YAW);
+		pitchMotor = Registrar.victor(RobotMap.Turret.PWM_PITCH);
 		
 		// Encoders
 		//yawEncoder = new Encoder(RobotMap.Turret.DIO_YAW_ENCODER_A, RobotMap.Turret.DIO_YAW_ENCODER_B);
